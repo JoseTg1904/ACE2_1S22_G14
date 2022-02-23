81f5f404bbc1e01b39aec893d3a2bee3c16c3913
José Carlos I Alonzo <2897107230101@ingenieria.usac.edu.gt>
2022-02-22 22:50:36 -0600

    lumens

--- Practica 1/Arduino/pra1.ino
+++ Practica 1/Arduino/pra1.ino
@@ -49,7 +49,7 @@ float calcularHumedad() {
 double calcularLumens() {
   int lectura = analogRead(pinLDR);
   double lux = 12500000 * pow(lectura, -1.4059);
-  return 0.09290304 * lux * 0.000215278;
+  return lux * 0.00002;
 }
 
 float calcularCO2() {
