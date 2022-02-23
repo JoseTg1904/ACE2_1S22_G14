fba4d25768d9ac93551bdfa8e10f69abc9c5adeb
José Carlos I Alonzo <2897107230101@ingenieria.usac.edu.gt>
2022-02-22 18:59:51 -0600

    arduino

---
+++ Practica 1/Arduino/pra1.ino
@@ -0,0 +1,74 @@
+#include <math.h>
+
+//valores definidos para el sensor de CO2
+#define RLOAD 10.0
+#define PARA 116.6020682
+#define PARB 2.769034857
+#define RZERO 76.63
+
+//pines de uso para los sensores
+const int pinTemperatura1 = A0;
+const int pinTemperatura2 = A1;
+const int pinHumedad = A2;
+const int pinLDR = A3;
+const int pinCO2 = A4;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinTemperatura1, INPUT);
+  pinMode(pinTemperatura2, INPUT);
+  pinMode(pinHumedad, INPUT);
+  pinMode(pinLDR, INPUT);
+}
+
+void loop() {
+  //lectura de sensores
+  double temperatura1 = calcularTemperatura(pinTemperatura1);
+  double temperatura2 = calcularTemperatura(pinTemperatura2);
+  float humedad = calcularHumedad();
+  double lumens = calcularLumens();
+  float CO2 = calcularCO2();
+
+  Serial.println(obtenerJson(temperatura1, temperatura2, humedad, lumens, CO2));
+  delay(1500);
+}
+
+double calcularTemperatura(int pin) {
+  int lectura = analogRead(pin);
+  double millivolts = (lectura / 1023.0) * 5000;
+  return millivolts / 10;
+}
+
+float calcularHumedad() {
+  int lectura = analogRead(pinHumedad);
+  if (lectura >= 700) { return 0.0; }
+  float porcentajeSeco =  (lectura * 100) / 700;
+  return 100.0 - porcentajeSeco;
+}
+
+double calcularLumens() {
+  int lectura = analogRead(pinLDR);
+  double lux = 12500000 * pow(lectura, -1.4059);
+  return 0.09290304 * lux * 0.0215278;
+}
+
+float calcularCO2() {
+  int lectura = analogRead(pinCO2);
+  float resistencia = ((1023./(float)lectura) * 5. - 1.)*RLOAD;
+  return PARA * pow((resistencia/RZERO), -PARB);
+}
+
+String obtenerJson(double temp1, double temp2, float hum, double lum, float co2) {
+  String jsonSalida = "{\"temperatura1: \"";
+  jsonSalida.concat(temp1);
+  jsonSalida +=  ",\"temperatura2: \"";
+  jsonSalida.concat(temp2);
+  jsonSalida +=  ",\"humedad: \"";
+  jsonSalida.concat(hum);
+  jsonSalida +=  ",\"lumens: \"";
+  jsonSalida.concat(lum);
+  jsonSalida +=  ",\"co2: \"";
+  jsonSalida.concat(lum);
+  jsonSalida += "}";
+  return jsonSalida;
+}
