c3d63d7d0fe5ee4ed47270b80597aa29b9867574
JoseTg1904 <2897107230101@ingenieria.usac.edu.gt>
2022-05-08 16:46:29 -0600

    arduino

--- Proyecto2/arduino/arduino.ino
+++ Proyecto2/arduino/arduino.ino
@@ -1,41 +1,26 @@
 #include <SoftwareSerial.h>
+#include <Stepper.h>
 
 #define RLOAD 10.0
 #define PARA 116.6020682
 #define PARB 2.769034857
 #define RZERO 76.63
+#define steps 100
 
 SoftwareSerial BT(2, 3); //rx, tx
+Stepper stepper(steps, 6, 7, 8, 9);
+Stepper stepperR(steps, 9, 8, 7, 6);
 const int pinCO2 = A1;
 const int pinTemperatura = A0;
-const int pinValvula1 = 6;
-const int pinValvula2 = 7;
-const int pinValvula3 = 8;
-const int pinValvula4 = 9;
 const int pinChispero = 5;
 
 //String lectura = "";
-
-int paso [8][4] = {
-  {1, 0, 0, 0},
-  {1, 1, 0, 0},
-  {0, 1, 0, 0},
-  {0, 1, 1, 0},
-  {0, 0, 1, 0},
-  {0, 0, 1, 1},
-  {0, 0, 0, 1},
-  {1, 0, 0, 1}
-};
 
 void setup() {
   Serial.begin(9600);
   BT.begin(9600);
   pinMode(pinTemperatura, INPUT);
   pinMode(pinCO2, INPUT);
-  pinMode(pinValvula1, OUTPUT);
-  pinMode(pinValvula2, OUTPUT);
-  pinMode(pinValvula3, OUTPUT);
-  pinMode(pinValvula4, OUTPUT);
   pinMode(pinChispero, OUTPUT);
 }
 
@@ -46,22 +31,12 @@ void loop() {
     if (lectura == '0') { digitalWrite(pinChispero, LOW); } 
     else if (lectura == '1') { digitalWrite(pinChispero, HIGH); } 
     else if (lectura == '2') { 
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(pinValvula1, paso[i][0]);
-        digitalWrite(pinValvula2, paso[i][1]);
-        digitalWrite(pinValvula3, paso[i][2]);
-        digitalWrite(pinValvula4, paso[i][3]);
-        delay(10);
-      }
+      stepper.setSpeed(30);
+      stepper.step(100);
     } 
     else if (lectura == '3') { 
-      for (int i = 8; i >= 0; i--) {
-        digitalWrite(pinValvula1, paso[i][3]);
-        digitalWrite(pinValvula2, paso[i][2]);
-        digitalWrite(pinValvula3, paso[i][1]);
-        digitalWrite(pinValvula4, paso[i][0]);
-        delay(10);
-      }
+      stepperR.setSpeed(30);
+      stepperR.step(100);
     }
   }
   if (BT.available() > 0) {
@@ -70,22 +45,10 @@ void loop() {
     if (lectura == '0') { digitalWrite(pinChispero, LOW); } 
     else if (lectura == '1') { digitalWrite(pinChispero, HIGH); } 
     else if (lectura == '2') { 
-      for (int i = 0; i < 8; i++) {
-        digitalWrite(pinValvula1, paso[i][0]);
-        digitalWrite(pinValvula2, paso[i][1]);
-        digitalWrite(pinValvula3, paso[i][2]);
-        digitalWrite(pinValvula4, paso[i][3]);
-        delay(10);
-      }
+
     } 
     else if (lectura == '3') { 
-      for (int i = 8; i >= 0; i--) {
-        digitalWrite(pinValvula1, paso[i][3]);
-        digitalWrite(pinValvula2, paso[i][2]);
-        digitalWrite(pinValvula3, paso[i][1]);
-        digitalWrite(pinValvula4, paso[i][0]);
-        delay(10);
-      }
+
     }
   }
   /*float temperatura = calcularTemperatura(pinTemperatura);
